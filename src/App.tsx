8585f61336cb9a31e31a4d1af579b77287904733
kimhsr <kimhsr@naver.com>
2022-12-20 16:04:19 +0900

    useSelector & useDispatch

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
+import { useSelector } from "react-redux/es/exports";
+import { useDispatch } from "react-redux/es/hooks/useDispatch";
 import "./App.css";
+import { RootState } from "./reducers";
 
 type Props = {
   value: any;
@@ -8,20 +11,29 @@ type Props = {
 };
 
 function App({ value, onIncrement, onDecrement }: Props) {
+  const dispatch = useDispatch();
+  const counter = useSelector((state: RootState) => state.counter);
+  const todos: string[] = useSelector((state: RootState) => state.todos);
+
   const [todoValue, setTodoValue] = useState("");
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setTodoValue(e.target.value);
   };
   const addTodo = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    dispatch({ type: "ADD_TODO", text: todoValue });
     setTodoValue("");
   };
   return (
     <div className="App">
-      {/* Clicked: {value} times */}
+      Clicked: {counter} times
       <button onClick={onIncrement}>+</button>
       <button onClick={onDecrement}>-</button>
-
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>{todo}</li>
+        ))}
+      </ul>
       <form onSubmit={addTodo}>
         <input type="text" value={todoValue} onChange={handleChange} />
         <input type="submit" />
